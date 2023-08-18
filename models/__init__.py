5dae73777e786a2cf29f883361293a03a18669b1
Yi-Ting Li <liyitingtom@gmail.com>
2023-08-18 15:41:18 -0400

    doc: update docs

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,17 @@
+"""
+models/__init__.py
+
+This module is responsible for making the necessary data models available at the package level.
+By importing the models here, they can be easily accessed from the 'models' package in other parts
+of the application without needing to know the specific module in which each model is defined.
+
+Author:
+    Yi-Ting Li (yitingli.public@gmail.com)
+
+Imports:
+    - MultimediaCreate: Model for validating the data when creating a new multimedia object.
+    - MultimediaInDBOutput: Model for representing the response data for a multimedia object.
+    - MultimediaInDB: Model for representing a multimedia object stored in the database.
+"""
+
 from models._multimedia import MultimediaCreate, MultimediaInDBOutput, MultimediaInDB
--- models/_multimedia.py
+++ models/_multimedia.py
@@ -1,10 +1,36 @@
+"""
+models/_multimedia.py
+
+This module defines the data models used in the FastAPI application for handling
+multimedia files. These models are used for parsing and validating the data associated
+with multimedia files uploaded by users.
+
+Author:
+    Yi-Ting Li (yitingli.public@gmail.com)
+
+Classes:
+    - MultimediaBase: The base model representing a multimedia file uploaded by a user.
+    - MultimediaCreate: Model for validating the data when creating a new multimedia object.
+    - MultimediaInDB: Model for representing a multimedia object stored in the database.
+    - MultimediaInDBOutput: Model for representing the response data for a multimedia object.
+"""
+
 from datetime import datetime
 from typing import Optional
-
 from pydantic import Field, BaseModel
 
 
 class MultimediaBase(BaseModel):
+    """
+    The base model representing a multimedia file uploaded by a user.
+    
+    Attributes:
+        user_id (str): The ID of the user who uploaded the file.
+        content_type (str): The content type of the file (e.g., "image/jpeg").
+        file_data (bytes): The binary data of the uploaded file.
+        filename (Optional[str]): The name of the uploaded file. Defaults to None.
+        metadata (Optional[dict]): Additional metadata associated with the file. Defaults to an empty dict.
+    """
     user_id: str
     content_type: str
     file_data: bytes
@@ -13,18 +39,43 @@ class MultimediaBase(BaseModel):
 
 
 class MultimediaCreate(MultimediaBase):
+    """
+    Model for validating the data when creating a new multimedia object.
+    
+    Inherits from MultimediaBase.
+    """
     ...
 
 
 class MultimediaInDB(MultimediaBase):
+    """
+    Model for representing a multimedia object stored in the database.
+    
+    Inherits from MultimediaBase.
+    
+    Attributes:
+        id (str): The unique ID of the multimedia object in the database.
+        created_at (datetime): The timestamp when the multimedia object was created. Defaults to the current UTC time.
+    """
     id: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
 
 class MultimediaInDBOutput(MultimediaBase):
+    """
+    Model for representing the response data for a multimedia object.
+    
+    Inherits from MultimediaBase.
+    
+    Attributes:
+        id (str): The unique ID of the multimedia object.
+        file_data (str): The binary data of the file, base64 encoded.
+        created_at (datetime): The timestamp when the multimedia object was created. Defaults to the current UTC time.
+    """
     id: str
-    file_data: str              # base64 encoded
+    file_data: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
+    
     class Config:
         arbitrary_types_allowed = True
         json_schema_extra = {
@@ -38,4 +89,3 @@ class MultimediaInDBOutput(MultimediaBase):
                 "created_at": "2020-08-19T00:00:00",
             },
         }
-
